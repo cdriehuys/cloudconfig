45277b58828f303d73765b1a322a5aa083631d2a
Chathan Driehuys <cdriehuys@gmail.com>
2016-10-25 18:18:52 -0400

    Add ability to read config from a file.

--- cloudconfig/main.py
+++ cloudconfig/main.py
@@ -1,14 +1,36 @@
+import configparser
 from io import BytesIO
 
 import boto3
 
 
-def get_bucket_name():
+LOCAL_CONFIG_FILE = '.cloudconfigrc'
+LOCAL_CONFIG_SECTION = 'cloudconfig'
+
+
+def get_bucket_name(local_config):
+    if local_config.get('bucket_name'):
+        return local_config.get('bucket_name')
+
     return input("Bucket name: ")
 
 
-def get_config_name():
+def get_config_name(local_config):
+    if local_config.get('config_name'):
+        return local_config.get('config_name')
+
     return input("Config name: ")
+
+
+def load_local_config():
+    """Read and return the local config file"""
+    config = configparser.ConfigParser()
+    config.read(LOCAL_CONFIG_FILE)
+
+    if LOCAL_CONFIG_SECTION in config:
+        return config[LOCAL_CONFIG_SECTION]
+
+    return {}
 
 
 def read_cloud_config(bucket_name, config_name):
@@ -33,7 +55,9 @@ def read_cloud_config(bucket_name, config_name):
 
 
 if __name__ == '__main__':
-    bucket_name = get_bucket_name()
-    config_name = get_config_name()
+    config = load_local_config()
+
+    bucket_name = get_bucket_name(config)
+    config_name = get_config_name(config)
 
     print(read_cloud_config(bucket_name, config_name))
